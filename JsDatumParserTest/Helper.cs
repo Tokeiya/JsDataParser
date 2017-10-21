42e8d9c8d33bc13d533edd5af237854c6408bec8
NetSeed <net_seed@hotmail.com>
2017-10-21 17:51:33 +0900

    impl unit test helper.

--- JsDatumParserTest/Helper.cs
+++ JsDatumParserTest/Helper.cs
@@ -1,25 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Parseq;
+using Xunit;
 
 namespace JsDatumParserTest
 {
     public static class Helper
     {
-	    public static void AreSuccsess(this IReply<char, IEnumerable<char>> reply, string expected)
+	    public static string BuildString(this IEnumerable<char> source)
 	    {
-		    throw new NotImplementedException();
+		    if (source == null) Assert.True(false, $"{nameof(BuildString)}({nameof(source)})is null");
+		    return source.Aggregate(new StringBuilder(), (b, c) => b.Append(c), b => b.ToString());
+	    }
+
+	    public static void AreSuccess(this IReply<char, IEnumerable<char>> reply, string expected)
+	    {
+			reply.IsNotNull();
+		    expected.IsNotNull();
+		    AreSuccess(reply, act => expected.SequenceEqual(act));
 	    }
 
 	    public static void AreSuccess(this IReply<char, IEnumerable<char>> reply, Func<IEnumerable<char>, bool> predicate)
 	    {
-		    throw new NotImplementedException();
+			reply.IsNotNull();
+			predicate.IsNotNull();
+
+		    reply.Case(
+			    _ =>
+			    {
+				    false.IsTrue();
+				    return 0;
+			    },
+			    seq =>
+			    {
+					predicate(seq).IsTrue();
+				    return 0;
+			    });
 	    }
 
 	    public static void AreFail(this IReply<char, IEnumerable<char>> reply)
 	    {
-		    throw new NotImplementedException();
+			reply.IsNotNull();
+
+		    reply.Case(_ =>
+		    {
+			    true.IsTrue();
+			    return 0;
+		    }, seq =>
+		    {
+			    Assert.False(true, seq.BuildString());
+			    return 0;
+		    });
 	    }
     }
 }

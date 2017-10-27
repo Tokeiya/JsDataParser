1272dad865713368fd7b336958581f7444323f9c
NetSeed <net_seed@hotmail.com>
2017-10-27 10:42:57 +0900

    implement VerifyExteinsion

--- JsDataParser/VerifyExtensions.cs
+++ JsDataParser/VerifyExtensions.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.Linq;
 using System.Text;
 using JsDataParser.Entities;
+// ReSharper disable InconsistentNaming
 
 namespace JsDataParser
 {
 	internal static class VerifyExtensions
 	{
+		private static readonly IdentifierTypes[] _identifierTypeses =
+			(IdentifierTypes[]) Enum.GetValues(typeof(IdentifierTypes));
 
+		private static readonly ValueTypes[] _valueTypes =
+			(ValueTypes[]) Enum.GetValues(typeof(ValueTypes));
 
 		public static bool Verify(this IdentifierTypes type)
-		{
-#warning Verify_Is_NotImpl
-			throw new NotImplementedException("Verify is not implemented");
-		}
+			=> _identifierTypeses.Any(x => x == type);
 
 		public static bool Verify(this ValueTypes type)
-		{
-#warning Verify_Is_NotImpl
-			throw new NotImplementedException("Verify is not implemented");
-		}
+			=> _valueTypes.Any(x => x == type);
 	}
 }

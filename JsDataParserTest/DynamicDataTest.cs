bbba89379cc8e5dd083fab517c9dfc97d6da855a
NetSeed <net_seed@hotmail.com>
2017-10-31 19:26:06 +0900

    add Dynamic test

---
+++ JsDataParserTest/DynamicDataTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JsDataParser.DataLoader;
+using Xunit;
+
+namespace JsDataParserTest
+{
+	public class DynamicDataTest
+	{
+		[Fact]
+		public void EnumTest()
+		{
+			var actual = DataLoader.LoadAsDynamic(".\\Samples\\itemdata.txt");
+
+			var cnt = 0;
+			foreach ((dynamic key,dynamic value) elem in actual)
+			{
+				cnt++;
+			}
+
+			cnt.Is(333);
+		}
+	}
+}

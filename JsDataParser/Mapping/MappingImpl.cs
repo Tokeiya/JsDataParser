35a194459a9f2193c618a8e838ada7508e38e280
NetSeed <net_seed@hotmail.com>
2017-11-02 21:54:10 +0900

    Inprogress.

---
+++ JsDataParser/Mapping/MappingImpl.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright (C) 2017 Hikotaro Abe <net_seed@hotmail.com> All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JsDataParser.Entities;
+
+namespace JsDataParser.Mapping
+{
+    internal class MappingImpl
+    {
+	    private readonly Type _mapTo;
+	    private readonly bool _canCreateInstance;
+
+
+	    public MappingImpl(Type mapTo)
+	    {
+#warning MappingImpl_Is_NotImpl
+		    throw new NotImplementedException("MappingImpl is not implemented");
+	    }
+
+	    public object Map(ObjectLiteralEntity entity)
+	    {
+#warning Map_Is_NotImpl
+		    throw new NotImplementedException("Map is not implemented");
+	    }
+    }
+}

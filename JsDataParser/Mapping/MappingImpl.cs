8dcae03cff5b665fd852ac9f0acc0f6cf8526262
NetSeed <net_seed@hotmail.com>
2017-11-02 23:10:04 +0900

    implement Verify. in progress.

--- JsDataParser/Mapping/MappingImpl.cs
+++ JsDataParser/Mapping/MappingImpl.cs
@@ -21,7 +21,9 @@
  */
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 using JsDataParser.Entities;
@@ -30,6 +32,41 @@ namespace JsDataParser.Mapping
 {
 	internal class MappingImpl
 	{
+		private static readonly Action<ValueEntity, object> Empty = (_, __) => { };
+
+		private static bool Verify(Type mapTo, ValueTypes mapFrom)
+		{
+			switch (mapFrom)
+			{
+				case ValueTypes.Array:
+					return mapTo.GetInterfaces().Any(x => x == typeof(IEnumerable));
+
+				case ValueTypes.Boolean:
+					return mapTo.IsAssignableFrom(typeof(bool));
+
+				case ValueTypes.Identity:
+				case ValueTypes.String:
+				case ValueTypes.Function:
+					return mapTo.IsAssignableFrom(typeof(string));
+
+				case ValueTypes.Integer:
+					return mapTo.IsAssignableFrom(typeof(int));
+
+				case ValueTypes.Real:
+					return mapTo.IsAssignableFrom(typeof(double));
+
+				case ValueTypes.Object:
+					return true;
+
+
+				default:
+					throw new InvalidOperationException($"{mapFrom} is unexpected.");
+			}
+		}
+
+
+
+
 		private readonly bool _hasDefaultCtor;
 
 		private readonly Dictionary<Type, MappingImpl> _candidates;
@@ -37,6 +74,8 @@ namespace JsDataParser.Mapping
 		private readonly Dictionary<(string name, ValueTypes type), Action<ValueEntity, object>> _cache
 			= new Dictionary<(string name, ValueTypes type), Action<ValueEntity, object>>();
 
+		private readonly PropertyInfo[] _properties;
+		private readonly FieldInfo[] _fields;
 
 
 		public MappingImpl(Type mapToType, params Type[] candidates)
@@ -47,15 +86,44 @@ namespace JsDataParser.Mapping
 				.ToDictionary(x => x, x => new MappingImpl(x));
 
 			_hasDefaultCtor = mapToType.GetConstructor(Array.Empty<Type>()) == null;
+
+			_properties = MapToType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+			_fields = mapToType.GetFields(BindingFlags.Public | BindingFlags.Instance);
 		}
 
 		public Type MapToType { get; }
 
 
+		private Action<ValueEntity, object> BuildProperty(PropertyInfo info, ValueTypes valueType)
+		{
+#warning BuildProperty_Is_NotImpl
+			throw new NotImplementedException("BuildProperty is not implemented");
+		}
+
+		private Action<ValueEntity, object> BuildFields(FieldInfo info, ValueTypes valueType)
+		{
+#warning BuildFields_Is_NotImpl
+			throw new NotImplementedException("BuildFields is not implemented");
+		}
+
+
+		[SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
 		private Action<ValueEntity, object> Build(string identity, ValueTypes valueType)
 		{
-#warning Build_Is_NotImpl
-			throw new NotImplementedException("Build is not implemented");
+			var propCandidate = _properties.Where(x => x.Name.ToLower() == identity)
+				.Where(x => Verify(x.PropertyType, valueType));
+
+			var fldCandidate = _fields.Where(x => x.Name.ToLower() == identity)
+				.Where(x => Verify(x.FieldType, valueType));
+
+			var cnt = propCandidate.Count() + fldCandidate.Count();
+
+			if (cnt > 1) throw new MappingException($"Too many candidates.");
+			if (cnt == 0) return Empty;
+
+			var prop = propCandidate.FirstOrDefault();
+
+			return prop != null ? BuildProperty(prop, valueType) : BuildFields(fldCandidate.First(), valueType);
 		}
 
 

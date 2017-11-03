3c4ff0985e6229e100ac8ccc60074310139edc70
NetSeed <net_seed@hotmail.com>
2017-11-03 20:18:51 +0900

    misc.

--- JsDataParserTest/DynamicDataTest.cs
+++ JsDataParserTest/DynamicDataTest.cs
@@ -1,4 +1,6 @@
-﻿using JsDataParser.DataLoader;
+﻿using System.Collections;
+using System.Collections.Generic;
+using JsDataParser.DataLoader;
 using Xunit;
 
 namespace JsDataParserTest
@@ -15,6 +17,24 @@ namespace JsDataParserTest
 				cnt++;
 
 			cnt.Is(333);
+		}
+
+		[Fact]
+		public void ReadScalarTest()
+		{
+			var loaded = DataLoader.LoadAsDynamic(".\\Samples\\DynamicMappingSamples.txt");
+
+
+			var actual = loaded[1];
+
+			Assert.NotNull(actual);
+
+
+			Assert.Equal<string>((string) actual.name, "12cm Single Cannon");
+
+			int hoge = actual.SLOTS[0];
+
+			Assert.True(true);
 		}
 	}
 }

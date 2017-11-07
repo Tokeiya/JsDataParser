3537fd8f5f8ac83b2a592e1f2b7bc4428d9013cf
NetSeed <net_seed@hotmail.com>
2017-11-07 16:19:23 +0900

    add MappingFromAttribute.

---
+++ JsDataParser/Mapping/MappingFromAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JsDataParser.Entities;
+
+namespace JsDataParser.Mapping
+{
+	[AttributeUsage(AttributeTargets.Property|AttributeTargets.Field,AllowMultiple = false)]
+	public sealed class MappingFromAttribute:Attribute
+	{
+		public MappingFromAttribute(string mappingFromidentity, bool isIdentity)
+		{
+			if (mappingFromidentity == null) throw new ArgumentNullException(nameof(mappingFromidentity));
+
+			MappingFromIdentifier = new IdentifierEntity(mappingFromidentity, isIdentity);
+		}
+
+		public MappingFromAttribute(int key)
+			=> MappingFromIdentifier = new IdentifierEntity(key);
+
+
+		public MappingFromAttribute(double key)
+			=> MappingFromIdentifier = new IdentifierEntity(key);
+
+		public MappingFromAttribute(bool key)
+			=> MappingFromIdentifier = new IdentifierEntity(key);
+
+
+		public IdentifierEntity MappingFromIdentifier { get; }
+
+	}
+}

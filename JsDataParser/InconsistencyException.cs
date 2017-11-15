ca636d5a1bc6af15106568b6e304eccc90b865f7
NetSeed <net_seed@hotmail.com>
2017-11-15 19:47:59 +0900

    add inconsistency exception.

---
+++ JsDataParser/InconsistencyException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsDataParser
+{
+	public class InconsistencyException : Exception
+	{
+		public InconsistencyException(string message) : base(message)
+		{
+		}
+
+		public InconsistencyException()
+		{
+		}
+
+		public InconsistencyException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
